6580f35c4ffa8ce89958d93a009fa548058cd329
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-03-16 15:42:20 +0900

    Update crawling_phone.py

--- crawling_phone.py
+++ crawling_phone.py
@@ -6,7 +6,7 @@ def get_browser(url):
     browser.get(url)
     browser.implicitly_wait(10)
 
-    box = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]').text # 와 텍스트 안붙여서 헤맨거야?
+    box = browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]').text
 
     return get_name_phone(browser, box)
 
@@ -14,10 +14,9 @@ def get_name_phone(browser, box):
     result = []
     try:
         for i in range(3, 3 + int(box)):
-            # 왜 23부터 못읽는지 -> time.sleep()이 충분히 길지 않아서
             name = browser.find_element_by_xpath(
                 '/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[2]/div[1]/div[' + str(
-                    i) + ']/a/div/div[2]/div').text  # 왜 됐다 안됐다 하노,,,
+                    i) + ']/a/div/div[2]/div').text
             phone = browser.find_element_by_xpath(
                 '/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[2]/div[1]/div[' + str(
                     i) + ']/a/div/div[3]/div/a/div/span').text
