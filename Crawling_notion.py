7052b136cbecc064762bd563a5963e70a8ff0310
tbnsok40 <tbnsok40@naver.com>
2021-03-22 16:25:02 +0900

    [최종] 합격자 합산 로직

--- Crawling_notion.py
+++ Crawling_notion.py
@@ -17,7 +17,7 @@ def get_name_phone(browser, box):
             # 왜 23부터 못읽는지 -> time.sleep()이 충분히 길지 않아서
             name = browser.find_element_by_xpath(
                 '/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[2]/div[1]/div[' + str(
-                    i) + ']/a/div/div[2]/div').text  # 왜 됐다 안됐다 하노,,,
+                    i) + ']/a/div/div[2]/div').text  # 왜 됐다 안됐다 하노,,, # 마우스 스크롤 밑으로 내려가지 않아서 -> (브라우저 팝업할 때) 스크롤 다운 로직 추가할 것
             phone = browser.find_element_by_xpath(
                 '/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div[19]/div[2]/div/div/div[2]/div[1]/div[' + str(
                     i) + ']/a/div/div[3]/div/a/div/span').text
